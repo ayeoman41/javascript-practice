37c132935844b5aad89c0bc938c52f5c61ad0881
Beez <ayeoman41@gmail.com>
2022-11-27 10:50:09 -0500

    TOP practice 1

--- initial.js
+++ initial.js
@@ -1,5 +1,10 @@
 console.log(23+97);
 console.log("I'm Javascript");
-alert('I love this')
 console.log(1+2+3+4+5+6);
-console.log()
+let x= (4+6+9)/(77);
+console.log(x)
+let a=10
+console.log(a)
+console.log(9*a)
+let b=7*a
+console.log(b)
