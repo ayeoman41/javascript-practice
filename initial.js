6ca7b9d5fff619be35a0eafa8f51f5f78fff53a5
Beez <ayeoman41@gmail.com>
2022-11-27 10:56:46 -0500

    Function practice

--- initial.js
+++ initial.js
@@ -8,3 +8,15 @@ console.log(a)
 console.log(9*a)
 let b=7*a
 console.log(b)
+let max= 57
+const actual= max-13
+let percentage= actual/max
+console.log(percentage)
+
+function add7(num) {
+    return num + 7
+  }
+ 
+  console.log(add7(5))
+  
+ 
